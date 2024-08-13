4b58bc5c3c274f0bd4f640ee15806abd50f331c8
lorax <142108266+lorax42@users.noreply.github.com>
2024-08-13 06:41:16 +0200

    fix formatting

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -145,11 +145,11 @@ require("lazy").setup({
     {
         'lervag/vimtex'
     },
-    
+
     -- telescope
     {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        -- or                              , branch = '0.1.x',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
 })
