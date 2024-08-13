98a98a41ad0a217a5d621bfb612c12913609ce2c
loraX <142108266+lorax42@users.noreply.github.com>
2024-08-13 17:18:57 +0200

    add fzf keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,9 @@
 -- define common options
+vim.g.mapleader = " "
+
 local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
+    noremap = true, -- non-recursive
+    silent = true,  -- do not show message
 }
 
 -----------------
@@ -31,6 +33,16 @@ vim.keymap.set('i', '<A-k>', '<ESC>ddkP==gi', opts)
 vim.keymap.set('i', '<A-UP>', '<ESC>ddkP==gi', opts)
 -- vim.keymap.set('n', '<ALT+j>', ':m+<CR>', opts)
 -- vim.keymap.set('n', '<ALT+k>', ':m-2<CR>', opts)
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fi', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>fz', builtin.builtin, {})
+vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
 
 -----------------
 -- Visual mode --
