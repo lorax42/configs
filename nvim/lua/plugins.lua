8bdd4552e3215b56df5ab604acfae579e4029b3e
lorax <142108266+lorax42@users.noreply.github.com>
2024-08-17 07:43:40 +0200

    add plugins to nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -152,5 +152,58 @@ require("lazy").setup({
             require('telescope').setup()
         end
     },
+
+    --[[{'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+
+        init = function()
+            vim.g.barbar_auto_setup = false
+        end,
+
+        opts = {
+            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+            -- animation = true,
+            -- insert_at_start = true,
+            -- …etc.
+        },
+
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    }, --]]
+
+    {
+        'RRethy/vim-illuminate'
+    },
+
+    {
+        "willothy/nvim-cokeline",
+        dependencies = {
+            "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
+            "nvim-tree/nvim-web-devicons", -- If you want devicons
+            -- "stevearc/resession.nvim"       -- Optional, for persistent history
+        },
+        config = true
+    },
+
+    -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        }
+    },
+
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+        config = function ()
+            require("ibl").setup()
+        end,
+    },
 })
 
