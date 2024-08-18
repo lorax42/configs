170222cfa7ae5fb3f635484e01a2114b091d60fd
loraX <142108266+lorax42@users.noreply.github.com>
2024-08-18 15:55:06 +0200

    ...

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -59,6 +59,7 @@ require("lazy").setup({
 		version = "v2.*",
         dependencies = { "rafamadriz/friendly-snippets" },
         config = function ()
+            --require("luasnip.loaders.from_vscode").lazy_load({paths = "~/.local/share/nvim/lazy/friendly-snippets/package.json"})
             require("luasnip.loaders.from_vscode").lazy_load()
         end
 	},
