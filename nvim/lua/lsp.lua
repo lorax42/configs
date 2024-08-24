7666b8f9fda4a81cef960ad6252f80426ead1205
loraX <142108266+lorax42@users.noreply.github.com>
2024-08-24 09:25:21 +0200

    move mson config from lsp.lua to plugins.lua in nvim/lua

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,16 +1,7 @@
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-})
-
 require('mason-lspconfig').setup({
     -- A list of servers to automatically install if they're not already installed
     ensure_installed = {
+        -- LSP SERVERS
         'pylsp',            -- Python
         'lua_ls',           -- Lua
         'rust_analyzer',    -- Rust
