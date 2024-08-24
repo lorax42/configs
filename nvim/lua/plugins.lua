f2d3d12fe3bf06dac5f27b14803a956b62809176
loraX <142108266+lorax42@users.noreply.github.com>
2024-08-24 09:25:21 +0200

    move mson config from lsp.lua to plugins.lua in nvim/lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -48,9 +48,24 @@ require("lazy").setup({
 		end,
 	},
 
+    -- LSP/DAP/Linter/Formatter Installer
+    {
+        "williamboman/mason.nvim",
+        config = function ()
+            require('mason').setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                }
+            })
+        end
+    },
+
     -- LSP manager
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
+    "williamboman/mason-lspconfig.nvim",
 
     {
         "neovim/nvim-lspconfig",
@@ -127,6 +142,57 @@ require("lazy").setup({
             }
         end
     },
+
+    ---- linter using language protokoll from mason
+    --{
+    --    "rshkarin/mason-nvim-lint",
+    --    config = function ()
+    --        require ('mason-nvim-lint').setup({
+    --            ensure_installed = {
+    --                'bacon',            -- bacon linter for rust is not available in nvim-lint, so it's specified to be directly installed from the mason's registry
+    --                'vale',             -- Markdown, LaTeX
+    --                'cpplint',          -- C/C++
+    --                'phpstan',          -- PHP
+    --                'proselint',        -- Text
+    --                'pylint',           -- Python
+    --
+    --                'clj-kondo',
+    --                'inko',
+    --                'ruby',
+    --                'janet',
+    --            },
+    --        })
+    --    end,
+    --},
+    --
+    ---- linter
+    --{
+    --    'mfussenegger/nvim-lint',
+    --    config = function ()
+    --        require('lint').linters_by_ft = {
+    --            markdown = {'vale',},
+    --            latex = {'vale',},
+    --            c = {'cpplint',},
+    --            cpp = {'cpplint',},
+    --            php = {'phpstan',},
+    --            text = {'proselint',},
+    --            python = {'pylint',},
+    --        }
+    --
+    --        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    --            callback = function()
+    --
+    --                -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    --                -- for the current filetype
+    --                require("lint").try_lint()
+    --
+    --                -- You can call `try_lint` with a linter name or a list of names to always
+    --                -- run specific linters, independent of the `linters_by_ft` configuration
+    --                --require("lint").try_lint("cspell")
+    --            end,
+    --        })
+    --    end
+    --},
 
     -- file tree
     {
