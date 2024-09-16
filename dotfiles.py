16444bf27cf6fcb66b6bacda946533cbeac84851
loraX <142108266+lorax42@users.noreply.github.com>
2024-09-16 08:43:50 +0200

    add __pycache__ to .gitignore

---
+++ dotfiles.py
@@ -0,0 +1,50 @@
+from funcs import *
+
+
+# list of functions and their identifier names
+funcs = [
+    ['exit', ex],
+    ['vim', vim],
+    ['nvim', nvim],
+    ['starship', starship],
+    ['kitty', kitty],
+    ['superfile', superfile],
+    ['sway', sway],
+    ['waybar', waybar],
+    ['wofi', wofi],
+]
+
+all_funcs = []
+for func in funcs[1:]:
+    all_funcs.append(func[1])
+
+# list of bundles
+bundles = [
+    [
+        'exit',
+        [
+            ex,
+        ]
+    ],
+    [
+        'base',
+        [
+            vim,
+            nvim,
+            starship,
+            kitty,
+        ]
+    ],
+    [
+        'sway',
+        [
+            sway,
+            waybar,
+            wofi,
+        ]
+    ],
+    [
+        'all',
+        all_funcs
+    ],
+]
