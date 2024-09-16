f610a467a15a37c435174eea2694ec6abc9a2b4a
loraX <142108266+lorax42@users.noreply.github.com>
2024-09-16 08:50:29 +0200

    split install.py up into files and add bundles

--- install.py
+++ install.py
@@ -2,73 +2,103 @@
 # install script for configs
 
 import sys
+import os
+import shutil
 
-from funcs import *
-from dotfiles import *
+
+# install files
+def install(src, dest):
+    if os.path.exists(dest) and os.path.exists(src):
+        if not input(f"overwrite {dest} (y to continue)") in "yY\n":
+            return 1
+    else:
+        print(f"E: src = {src}; dest = {dest}; one or both of the files don't exist")
+        return 1
+
+    print(f"installing {src} => {dest}")
+    if os.path.isfile(dest) and os.path.isfile(src):
+        os.remove(dest)
+        shutil.copyfile(src, dest)
+    elif os.path.isdir(dest) and os.path.isdir(src):
+        shutil.rmtree(dest)
+        shutil.copytree(src, dest)
+    else:
+        print(f"E: src = {src}; dest = {dest}; file type not matching")
+        sys.exit(1)
+
+    print("-------------------------")
+
+    return 0
+
+
+# function for vim
+def vim():
+    install('./.vimrc', '~/.vimrc')
+
+
+# function for starship
+def starship():
+    install('./starship.toml', '~/.config/starship.toml')
+
+
+# function for nvim
+def nvim():
+    install('./nvim/', '~/.config/nvim/')
+
+
+# function for kitty
+def kitty():
+    install('./kitty/', '~/.config/kitty/')
+
+
+# function for sway
+def sway():
+    install('./sway/', '~/.config/sway/')
+
+
+# function for superfile
+def superfile():
+    install('./superfile/', '~/.config/superfile/')
+
+
+# exit program
+def ex():
+    sys.exit(1)
 
 
 # DRIVER
 def main():
+    # list of functions and their identifier names
+    funcs = [
+        [ex, 'exit']
+        [vim, 'vim'],
+        [nvim, 'nvim'],
+        [starship, 'starship'],
+        [kitty, 'kitty'],
+        [superfile, 'superfile'],
+        [sway, 'sway'],
+    ]
+
     # if no args select from list
     if len(sys.argv) < 2:
-        print("0: exit")
-        print("1: dotfile")
-        print("2: bundle")
+        i = 0
+        for _, name in funcs:
+            print(f"{i}: {name}")
+            i += 1
 
-        index = int(input("> "))
-
-        if index == 0:
-            ex()
-
-        elif index == 1:
-            i = 0
-
-            for func in funcs:
-                print(f"{i}: {func[0]}")
-                i += 1
-
-            index = int(input("> "))
-
-            if index > len(funcs) or index < 0:
-                print(f"E: invalid index = {index}")
-                ex()
-
-            funcs[index][1]()
-
-        elif index == 2:
-            i = 0
-
-            for bundle in bundles:
-                print(f"{i}: {bundle[0]}")
-                i += 1
-
-            index = int(input("> "))
-
-            if index > len(bundles) or index < 0:
-                print(f"E: invalid index = {index}")
-                ex()
-
-            for func in bundles[index][1]:
-                func[1]()
-
-        else:
+        index = input("> ")
+        if index > len(funcs):
             print(f"E: invalid index = {index}")
+            sys.exit(1)
 
-    elif sys.argv[1] == "b" or sys.argv[1] == "-b":
-        for arg in sys.argv:
-            # loop through bunles in list (skipping first (exit))
-            print(bundles[1:])
-            for bundle in bundles[1:]:
-                if bundle[0] == arg:
-                    for func in bundle[1]:
-                        func()
+        op = funcs[index][0]
+        op()
 
-    else:
-        # search args for installable dotfiles
-        for arg in sys.argv:
-            for func in funcs:
-                if arg == func[0]:
-                    func[1]()
+    # search args for installable dotfiles
+    for arg in sys.argv:
+        for func, name in funcs:
+            if arg == name:
+                func()
 
 
 if __name__ == "__main__":
