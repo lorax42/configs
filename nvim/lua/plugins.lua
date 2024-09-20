ca756939be36924c2f73daceb9d12eb0d4a23df0
loraX <142108266+lorax42@users.noreply.github.com>
2024-09-20 17:53:22 +0200

    update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -368,5 +368,16 @@ require("lazy").setup({
             })
         end,
     },
+
+    --{
+    --    'nvimdev/dashboard-nvim',
+    --    event = 'VimEnter',
+    --    config = function()
+    --        require('dashboard').setup {
+    --            -- config
+    --        }
+    --    end,
+    --    dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    --},
 })
 
