2fa37efc1e623546ece1d59714cf57a5779d4726
lorax <142108266+lorax42@users.noreply.github.com>
2024-10-13 20:56:41 +0200

    update nvim/plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -413,11 +413,18 @@ require("lazy").setup({
     --    dependencies = { {'nvim-tree/nvim-web-devicons'}}
     --},
 
+    -- undotree
     {
         'mbbill/undotree',
     },
 
+    -- better rust integration
     {
         'rust-lang/rust.vim',
     },
+
+    -- peek definitions, implementations, references…
+    -- {
+    --     'dnlhc/glance.nvim'
+    -- },
 })
