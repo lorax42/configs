36b3f5a197e5d9015257eefeeb5e60e430292ebc
loraX <142108266+lorax42@users.noreply.github.com>
2024-10-14 09:08:40 +0200

    add whichley to nvim and add keybinds

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -43,6 +43,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>flr', builtin.lsp_references, {})
 
 -- NERDTree
 --vim.keymap.set('n', '<C-f>', ':NERDTree<CR>', {})
@@ -84,22 +85,22 @@ map("n", "<Leader>p", "<Plug>(cokeline-switch-prev)", { silent = true })
 map("n", "<Leader>n", "<Plug>(cokeline-switch-next)", { silent = true })
 
 for i = 1, 9 do
-  map(
-    "n",
-    ("<F%s>"):format(i),
-    ("<Plug>(cokeline-focus-%s)"):format(i),
-    { silent = true }
-  )
-  map(
-    "n",
-    ("<Leader>%s"):format(i),
-    ("<Plug>(cokeline-switch-%s)"):format(i),
-    { silent = true }
-  )
+    map(
+        "n",
+        ("<F%s>"):format(i),
+        ("<Plug>(cokeline-focus-%s)"):format(i),
+        { silent = true }
+    )
+    map(
+        "n",
+        ("<Leader>%s"):format(i),
+        ("<Plug>(cokeline-switch-%s)"):format(i),
+        { silent = true }
+    )
 end
 
 -- lsp
-vim.keymap.set('n', '<leader>e', function()     vim.diagnostic.open_float(nil, { focusable = false })  end)
+vim.keymap.set('n', '<leader>e', function() vim.diagnostic.open_float(nil, { focusable = false }) end)
 
 -----------------
 -- Visual mode --
