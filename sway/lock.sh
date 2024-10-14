5c9f6076a9be3615795f64777f3363b8c67c1d09
loraX <142108266+lorax42@users.noreply.github.com>
2024-10-14 22:50:31 +0200

    add blurry lock

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,8 +1,30 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
+echo "running lock.sh..."
+
+img=~/.config/sway/img/Cyberpunk-Background-HD-2357151896.jpg
+
+if [[ "$1" == "blur" ]]; then
+    echo "running lockimg.sh..."
+    ~/.config/sway/lockimg.sh
+    if [[ $? ]]; then
+        echo "using img/lockimg.png..."
+        img=~/.config/sway/img/lockimg.png
+    else
+        echo "using fallback img..."
+    fi
+fi
+
+if [[ ! -f $img ]]; then
+    echo "E: $img not found"
+    exit 1
+fi
+
+echo "running swaylock..."
 swaylock\
     --show-failed-attempts\
     --indicator-radius 75\
-    --image ~/.config/sway/img/Cyberpunk-Background-HD-2357151896.jpg\
+    --image $img\
     --scaling fit\
+    --indicator-caps-lock\
     --config ~/.config/sway/swaylock.conf\
