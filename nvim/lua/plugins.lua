165e918ad9cb465bccb90e806bd55932620e0cc1
loraX <142108266+lorax42@users.noreply.github.com>
2024-10-20 12:29:44 +0200

    update lspsaga plugin for nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -74,7 +74,11 @@ require("lazy").setup({
             'nvimdev/lspsaga.nvim',
 
             config = function()
-                require('lspsaga').setup({})
+                require('lspsaga').setup({
+                    ui = {
+                        code_action = ""
+                    }
+                })
             end,
 
             dependencies = {
