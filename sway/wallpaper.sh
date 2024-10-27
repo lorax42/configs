6a3dbc5c5c0b266453e69f28917c70a45d4f7bda
lorax <142108266+lorax42@users.noreply.github.com>
2024-10-27 13:10:37 +0100

    update sway/wallpaper.sh

--- sway/wallpaper.sh
+++ sway/wallpaper.sh
@@ -5,7 +5,7 @@ local_wp_dir=~/Pictures/wallpaper
 img=~/.config/sway/img/Cyberpunk-Background-HD-2357151896.jpg
 
 echo "checking for local wallpaper directory..."
-if [[ -d $local_wp_dir ]]; then
+if [[ -f $local_wp_dir/index.txt ]]; then
     echo "reading $local_wp_dir/index.txt..."
     index="$local_wp_dir/$(cat $local_wp_dir/index.txt | head -n1)"
 
