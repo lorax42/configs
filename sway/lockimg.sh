d10eae7e68af36184b2a4e453e08dc3fff383e78
lorax <142108266+lorax42@users.noreply.github.com>
2024-11-11 06:40:35 +0100

    change sway/lockimg.sh blur method

--- sway/lockimg.sh
+++ sway/lockimg.sh
@@ -17,8 +17,10 @@ fi
 echo "v"
 
 echo "using grim..."
-grim -s0.5 ~/.config/sway/img/lockimg.png
+# grim -s0.5 ~/.config/sway/img/lockimg.png
+grim ~/.config/sway/img/lockimg.png
 echo "returned $?"
 echo "using convert..."
-convert ~/.config/sway/img/lockimg.png -blur 1 ~/.config/sway/img/lockimg.png
+# convert ~/.config/sway/img/lockimg.png -blur 1 ~/.config/sway/img/lockimg.png
+convert ~/.config/sway/img/lockimg.png -scale 20x20% -scale 500x500% -blur 1 ~/.config/sway/img/lockimg.png
 echo "returned $?"
