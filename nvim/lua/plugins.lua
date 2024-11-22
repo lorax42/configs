8542bc5b73f64f972aedd0e157ba78510f9e59d2
lorax <142108266+lorax42@users.noreply.github.com>
2024-11-22 07:06:38 +0100

    add color picker plugin to nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -451,4 +451,12 @@ require("lazy").setup({
             },
         },
     },
+    
+    {
+        "max397574/colortils.nvim",
+        cmd = "Colortils",
+        config = function()
+            require("colortils").setup()
+        end,
+    },
 })
