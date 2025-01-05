18abd9a811305f044243fd42564de61ee60e48e0
lorax <142108266+lorax42@users.noreply.github.com>
2025-01-05 10:55:27 +0100

    add lsp based colorizer to nvim

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -41,8 +41,8 @@ cmp.setup({
             -- Hint: if the completion menu is visible select next one
             if cmp.visible() then
                 cmp.select_next_item()
-            --elseif luasnip.expand_or_jumpable() then
-            --    luasnip.expand_or_jump()
+                --elseif luasnip.expand_or_jumpable() then
+                --    luasnip.expand_or_jump()
             elseif has_words_before() then
                 cmp.complete()
             else
@@ -65,38 +65,39 @@ cmp.setup({
         end, { "i", "s" }),
     }),
 
-  -- Let's configure the item's appearance
-  -- source: https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
-  formatting = {
-      -- Set order from left to right
-      -- kind: single letter indicating the type of completion
-      -- abbr: abbreviation of "word"; when not empty it is used in the menu instead of "word"
-      -- menu: extra text for the popup menu, displayed after "word" or "abbr"
-      fields = { 'abbr', 'menu' },
+    -- Let's configure the item's appearance
+    -- source: https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
+    formatting = {
+        -- Set order from left to right
+        -- kind: single letter indicating the type of completion
+        -- abbr: abbreviation of "word"; when not empty it is used in the menu instead of "word"
+        -- menu: extra text for the popup menu, displayed after "word" or "abbr"
+        fields = { 'abbr', 'menu' },
 
-      -- customize the appearance of the completion menu
-      format = function(entry, vim_item)
-          vim_item.menu = ({
-              -- nvim_lsp = '[Lsp]',
-              nvim_lsp = '󰿘',
-              -- luasnip = '[Luasnip]',
-              luasnip = '',
-              -- buffer = '[File]',
-              buffer = '',
-              -- path = '[Path]',
-              path = '',
-          })[entry.source.name]
-          return vim_item
-      end,
-  },
+        -- customize the appearance of the completion menu
+        format = function(entry, vim_item)
+            vim_item.menu = ({
+                -- nvim_lsp = '[Lsp]',
+                nvim_lsp = '󰿘',
+                -- luasnip = '[Luasnip]',
+                luasnip = '',
+                -- buffer = '[File]',
+                buffer = '',
+                -- path = '[Path]',
+                path = '',
+            })[entry.source.name]
+            return vim_item
+        end,
+        format = require("nvim-highlight-colors").format
+    },
 
-  -- Set source precedence
-  sources = cmp.config.sources({
+    -- Set source precedence
+    sources = cmp.config.sources({
         { name = 'luasnip' },     -- For luasnip user
         { name = 'nvim_lsp' },    -- For nvim-lsp
         { name = 'vimtex' },      -- vimtex support [added by me]
         { name = 'buffer' },      -- For buffer word completion
         { name = 'path' },        -- For path completion
-  })
+    })
 })
 
