f46cf172b2225008c8c8cd9207c08adc556c4958
lorax <142108266+lorax42@users.noreply.github.com>
2025-01-30 10:11:12 +0100

    add ai autocompletion

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -498,4 +498,21 @@ require("lazy").setup({
     {
         "xiyaowong/transparent.nvim"
     },
+
+    {
+        'codota/tabnine-nvim',
+        config = function ()
+            require('tabnine').setup({
+                disable_auto_comment=true,
+                accept_keymap="<C-a>",
+                dismiss_keymap = "<C-]>",
+                debounce_ms = 800,
+                suggestion_color = {gui = "#808080", cterm = 244},
+                exclude_filetypes = {"TelescopePrompt", "NvimTree"},
+                log_file_path = nil, -- absolute path to Tabnine log file
+                ignore_certificate_errors = false,
+            })
+        end,
+        build = "./dl_binaries.sh"
+    },
 })
