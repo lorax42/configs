992536be471159e3f88c9a766d41d1cb14bad0a4
lorax <142108266+lorax42@users.noreply.github.com>
2025-01-30 06:54:49 +0100

    update nvim diagnostics settings
    - not float overlay on insert mode

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -13,14 +13,14 @@ for type, icon in pairs(signs) do
 end
 
 vim.o.updatetime = 250
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+vim.api.nvim_create_autocmd({ "CursorHold"--[[ , "CursorHoldI" ]] }, {
   group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
   callback = function ()
     vim.diagnostic.open_float(nil, {focus=false})
   end
 })
 
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+vim.api.nvim_create_autocmd({ "CursorHold"--[[ , "CursorHoldI" ]] }, {
   group = vim.api.nvim_create_augroup("float_diagnostic_cursor", { clear = true }),
   callback = function ()
     vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})
