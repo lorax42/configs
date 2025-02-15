f1b3fd76eae745d3a6b597f6692e4c9d9fcf2f38
lorax <142108266+lorax42@users.noreply.github.com>
2025-02-15 14:06:19 +0100

    Add session manager to nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -519,4 +519,17 @@ require("lazy").setup({
         end,
         build = "./dl_binaries.sh"
     },
+
+    {
+        'rmagatti/auto-session',
+        lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            suppressed_dirs = { '~/', '~/Downloads', '/' },
+            -- log_level = 'debug',
+        }
+    }
 })
