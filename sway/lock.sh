3d165a0b7c5b2472c6d4198fc72abd04ecf8834e
lorax <142108266+lorax42@users.noreply.github.com>
2025-03-11 11:28:24 +0100

    feat(sway): Add config for swaylock-effects

--- sway/lock.sh
+++ sway/lock.sh
@@ -21,10 +21,22 @@ if [[ ! -f $img ]]; then
 fi
 
 echo "running swaylock..."
-swaylock\
-    --show-failed-attempts\
-    --indicator-radius 75\
-    --image $img\
-    --scaling fit\
-    --indicator-caps-lock\
-    --config ~/.config/sway/swaylock.conf &
+# swaylock\
+#     --show-failed-attempts\
+#     --indicator-radius 75\
+#     --image $img\
+#     --scaling fit\
+#     --indicator-caps-lock\
+#     --config ~/.config/sway/swaylock.conf &
+
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--grace 2 \
+	--fade-in 0.2 \
+    --config ~/.config/sway/swaylock.conf
